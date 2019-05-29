6136db9ccca02eb4ad8abe425822560ea4636831
HexRabbit <mikuqb8718@gmail.com>
2019-05-30 07:24:42 +0800

    狂おしいほど僕には愛おしい
    - change python interpreter in .vimrc to python3.6
    - rearrange some script
    - auto set locale
    - add `--depth=1` to all `git clone` command
    - fix typo

--- install_all.sh
+++ install_all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SUDO=""
+
 # don't run script in root unless you know what you're doing
 if [ $USER == "root" ]; then
   echo "You're running the script with user root,"
@@ -12,23 +14,32 @@ if [ $USER == "root" ]; then
   done
 fi
 
-# preliminary
-if [ $USER == "root" ]; then
-  apt-get update
-  apt-get install -y build-essential cmake python3-dev python3-pip vim zsh tmux git curl sed 
-else
-  sudo apt-get update
-  sudo apt-get install -y build-essential cmake python3-dev python3-pip vim zsh tmux git curl sed 
+if [ $USER != "root" ]; then
+  SUDO=sudo
 fi
+
+# install dependency
+$SUDO apt-get update
+$SUDO apt-get install -y build-essential cmake python3-dev python3-pip vim-gtk3 zsh tmux git curl sed
+
+# setup locale
+$SUDO locale-gen en_US.UTF-8
+$SUDO update-locale LANG=en_US.UTF-8
 
 # vim
 git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 cp .vimrc $HOME
+sed -i "s/Plugin 'Valloric\/YouCompleteMe'/\"@@@/" $HOME/.vimrc
 vim -E -s -u "$HOME/.vimrc" +PluginInstall +qall
+sed -i "s/\"@@@/Plugin 'Valloric\/YouCompleteMe'/" $HOME/.vimrc
 cp molokai.vim $HOME/.vim/bundle/molokai/colors/molokai.vim 
 
 # YouCompleteMe (not complete, checkout YCM github for installation details)
-python3 $HOME/.vim/bundle/YouCompleteMe/install.py --clang-completer
+git clone --depth=1 https://github.com/Valloric/YouCompleteMe $HOME/.vim/bundle/YouCompleteMe
+cd $HOME/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+python3 ./install.py --clang-completer
+cd -
 #`gcc -print-prog-name=cc1` -v
 #`gcc -print-prog-name=cc1plus` -v
 
@@ -39,7 +50,7 @@ sed -i "/export ZSH=/c export ZSH=$HOME/.oh-my-zsh" $HOME/.zshrc
 sed -i 's/ZSH_THEME=.*/ZSH_THEME="clean"/' $HOME/.zshrc
 sed -i 's/plugins=(\(.*\))/plugins=(\1 zsh-completions)/' $HOME/.zshrc
 sed -i '/plugins=/a autoload -U compinit && compinit' $HOME/.zshrc
-git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
+git clone --depth=1 https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
 chsh -s $(which zsh)
 
 # tmux & powerline
